2541e3d58e20ed888c09bf0612db31922277492b
AXT-web <m18775921835@163.com>
2021-04-29 10:17:46 +0800

    完成了黑马day7的内容

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import GoodList from '../components/goods/List.vue'
-// 在router.js中引入goods/Add.vue,并添加路由规则
-import GoodAdd from './components/goods/Add.vue'
-import { component } from 'vue/types/umd'
+const Login = () => import(/* webpackChunkName:"login_home_welcome" */ './components/Login.vue')
+// import Login from './components/Login.vue'
+const Home = () => import(/* webpackChunkName:"login_home_welcome" */ './components/Home.vue')
+// import Home from './components/Home.vue'
+const Welcome = () => import(/* webpackChunkName:"login_home_welcome" */ './components/Welcome.vue')
+// import Welcome from './components/Welcome.vue'
+const Users = () => import(/* webpackChunkName:"user" */ './components/user/Users.vue')
+// import Users from './components/user/Users.vue'
+const Rights = () => import(/* webpackChunkName:"power" */ './components/power/Rights.vue')
+// import Rights from './components/power/Rights.vue'
+const Roles = () => import(/* webpackChunkName:"power" */ './components/power/Roles.vue')
+// import Roles from './components/power/Roles.vue'
+const Cate = () => import(/* webpackChunkName:"goods" */ './components/goods/Cate.vue')
+// import Cate from './components/goods/Cate.vue'
+const Params = () => import(/* webpackChunkName:"goods" */ './components/goods/Params.vue')
+// import Params from './components/goods/Params.vue'
+const GoodList = () => import(/* webpackChunkName:"goods" */ './components/goods/List.vue')
+// import GoodList from './components/goods/List.vue'
+const GoodAdd = () => import(/* webpackChunkName:"goods" */ './components/goods/Add.vue')
+// import GoodAdd from './components/goods/Add.vue'
+const Order = () => import(/* webpackChunkName:"order" */ './components/order/Order.vue')
+// import Order from './components/order/Order.vue'
+const Report = () => import(/* webpackChunkName:"report" */ './components/report/Report.vue')
+// import Report from './components/report/Report.vue'
 
 Vue.use(Router)
 
@@ -30,8 +42,10 @@ const router = new Router({
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
         { path: '/params', component: Params },
-        { path: '/goods', component: GoodList  },
-        { path: '/goods/add', component: GoodAdd}
+        { path: '/goods', component: GoodList },
+        { path: '/goods/add', component: GoodAdd },
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
       ]
     }
   ]
